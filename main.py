038bd2e43a9077c424295e7a87267eea308349e3
LauraCarvajal21 <lvcarvajalv@correo.udistrital.edu.co>
2022-06-20 23:05:51 -0500

    Inicie LCD

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import LCDLIB16x2 as LCD
+
+# Initialise display
+LCD.lcd_init()
+
+while True:
+
+    # Send some test
+    LCD.lcd_string(" Rasbperry Pi ", LCD.LINE_1)
+    LCD.lcd_string(" Hola bebe ", LCD.LINE_2)
+
+    time.sleep(3)  # 3 second delay
+
+    # Send some text
+    LCD.lcd_string("PWM", LCD.LINE_1)
+    LCD.lcd_string("#VUELTAS", LCD.LINE_2)
+
+    time.sleep(3)  # 3 second delay
+
+GPIO.cleanup()
